ba1e791c234174bce820f8ee1553cb643195f9ac
Zerumi <evgexa.2015@bk.ru>
2019-09-13 19:56:37 +0400

    version 1.0.2
    -Critical Error Fix

--- bot.js
+++ bot.js
@@ -215,7 +215,7 @@ else if(message.content.startsWith("!uinfo")){
           url: user.avatarURL
         }
       }})
-    }).catch(message.channel.send("Не удается найти пользователя. FAQ: https://support.discordapp.com/hc/ru/articles/206346498-Где-мне-найти-ID-пользователя-сервера-сообщения-"))
+    })
   } else {
     message.channel.send({ embed: {
       title: "**Информация о пользователе**: " + user.tag,
@@ -373,7 +373,7 @@ else if (message.content.startsWith("!end vote"))
   }});
   message.delete();
   return r.clearReactions();
-}).catch(message.channel.send("Не удалось найти сообщение. FAQ: https://support.discordapp.com/hc/ru/articles/206346498-%D0%93%D0%B4%D0%B5-%D0%BC%D0%BD%D0%B5-%D0%BD%D0%B0%D0%B9%D1%82%D0%B8-ID-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D0%B0-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D1%8F-"))
+})
 }
 // FIX
 if (message.content.startsWith("!osu user")) {
